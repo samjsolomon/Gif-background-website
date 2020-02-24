c6e1882240f1ecc30f28adba070619fba370837d
samjsolomon <58637477+samjsolomon@users.noreply.github.com>
2020-02-24 14:35:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import "./assets /css /bootstrap.css"; 
+import "./assets /css/bootstrap.css"; 
 import "./assets /css /agency.min.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
